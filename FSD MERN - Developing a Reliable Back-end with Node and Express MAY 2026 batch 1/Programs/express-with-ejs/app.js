52587abfd5fd16fe36db0e6f9265d05c7119ae25
Akash <akash300383@gmail.com>
2026-05-30 20:13:07 +0530

    done

--- FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/express-with-ejs/app.js
+++ FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/express-with-ejs/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 
 // set view engine
 app.set("view engine", "ejs");
+
+// set custom views directory
+//app.set("views", "./my_views");
 
 // middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
@@ -23,9 +26,10 @@ app.get("/login", (request, response) => {
 
 // in memory login data
 let logins = [
-    { email: "raj@gmail.com", password: "raj123" },
-    { email: "ravi@gmail.com", password: "ravi123" },
-    { email: "john@gmail.com", password: "john123" }
+    {email: "admin@gmail.com", password: "admin123", typeofUser: "admin"},
+    { email: "raj@gmail.com", password: "raj123", typeofUser: "user" },
+    { email: "ravi@gmail.com", password: "ravi123", typeofUser: "user" },
+    { email: "john@gmail.com", password: "john123", typeofUser: "user" }
 ];
 
 // check login credentials with hardcoded values
@@ -44,12 +48,36 @@ app.post("/checkLogin", (request, response) => {
     const login = request.body;
     let isValidUser = logins.some(user => user.email === login.email && user.password === login.password);
     if(isValidUser) {
-       response.render("dashboard", { msg: login.email });
+        if(login.typeofUser === "admin") {
+            response.render("adminDashboard", { msg: login.email });
+        } else {    
+       response.render("userDashboard", { msg: login.email });
+        }
     } else {
         response.render("login", { msg: "Invalid credentials. Please try again." });
     }
 });
 
+
+// get method 
+app.get("/addData", (request, response) => {
+    let msg = "";
+    response.render("addData",{msg});
+})
+
+// post method 
+app.post("/addData", (request, response) => {
+    const data = request.body;
+    let dataFromFs = JSON.parse(fs.readFileSync("data.json").toString());
+    dataFromFs.push(data);
+    fs.writeFileSync("data.json", JSON.stringify(dataFromFs));
+    response.render("addData", { msg: "Data added successfully!" });
+});
+
+app.get("/viewData", (request, response) => {
+    let dataFromFs = JSON.parse(fs.readFileSync("data.json").toString());
+    response.render("viewData",{data: dataFromFs});
+}); 
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
